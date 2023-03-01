08312ad06fabcce0a66fe2ebeadc5b39bd2043c6
ManupaDev <manupadevsuite@gmail.com>
2023-03-01 18:39:37 +0530

    feat:added react query data fetching for timeblocks home

--- src/routes/home/selected-day.tsx
+++ src/routes/home/selected-day.tsx
@@ -2,10 +2,32 @@ import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { DateContext } from "../../contexts/DateContext";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-
+import { useQuery } from "react-query";
+import { getAllTimeblocks } from "../../api/api";
 
 function SelectedDay() {
   const { selectedDate } = useContext(DateContext);
+  const { isLoading, isError, data, error, isSuccess } = useQuery("timeblocks", getAllTimeblocks);
+
+
+  if (isLoading) {
+    return (
+      <div className="mt-4 border-black">
+        <h1 className="text-2xl font-semibold">Today</h1>
+        <div className="mt-4 px-4">
+          <NavLink to={"/home/timeblocks/create"}>
+            <div className="border border-black flex justify-center px-2 py-2 rounded">
+              <PlusCircleIcon width={20} height={20} />
+            </div>
+          </NavLink>
+
+          <div className="grid grid-cols-1 place-items-center border-black mt-2">
+            <span className="block">Loading</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="mt-4 border-black">
@@ -17,12 +39,15 @@ function SelectedDay() {
           </div>
         </NavLink>
 
-        <div className="grid grid-cols-1 border-black mt-2">
-          <div className="border border-black px-4 py-2 rounded">
-            <span className="block">SDGP Implementation</span>
-          </div>
+        <div className="grid grid-cols-1 gap-y-2 mt-4 border-black">
+          {data.map((timeblock:any) => {
+            return (
+              <div key={timeblock.id} className="border border-black px-4 py-2 rounded">
+                <span className="block">{timeblock.name}</span>
+              </div>
+            );
+          })}
         </div>
-
       </div>
     </div>
   );

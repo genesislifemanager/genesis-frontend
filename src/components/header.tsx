ede81ba424c5258009c21ec24f2bbbc6cb9fabaf
ManupaDev <manupadevsuite@gmail.com>
2023-03-03 23:14:35 +0530

    feat: header component with dynamic timer

---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+dayjs.extend(advancedFormat);
+
+function Header() {
+  const [time, setTime] = useState(dayjs());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(dayjs());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="border border-black py-4 px-4 rounded-lg">
+      <span className="block text-base font-semibold">Hey, Sithmini</span>
+      <div>
+        <span className="block font-semibold text-2xl">{time.format("HH:mm:ss")}</span>
+        <span className="block font-semibold text-xl">{dayjs().format("Do MMMM YYYY")}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,17 +1,12 @@
 import { HomeIcon,Bars3Icon,Cog6ToothIcon } from "@heroicons/react/24/outline";
 import { useEffect } from "react";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import Header from "../components/header";
 
 function Root() {
   return (
     <div className="px-4 py-4 min-h-screen border relative border-black">
-      <div className="border border-black py-4 px-4 rounded-lg">
-        <span className="block text-base font-semibold">Hey, Sithmini</span>
-        <div>
-          <span className="block font-semibold text-2xl">18:04</span>
-          <span className="block font-semibold text-xl">24th March 2023</span>
-        </div>
-      </div>
+      <Header/>
       <Outlet />
       <div className="flex justify-center rounded-lg left-4 gap-x-16 right-4 bottom-4 absolute py-2 px-4 border border-black">
         <NavLink to={"/organize/projects"}>

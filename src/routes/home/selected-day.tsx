6f93b2b3191e553292224731a4c8078028f62010
Gayali Alahakoon <124614806+GayaliAlahakoon@users.noreply.github.com>
2023-03-30 15:39:57 +0530

    adding colors for timeblock cards

--- src/routes/home/selected-day.tsx
+++ src/routes/home/selected-day.tsx
@@ -1,4 +1,6 @@
-import { PlusCircleIcon, ChevronRightIcon, CheckCircleIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon, PlusIcon, ChevronRightIcon} from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+
 import { DateContext } from "../../contexts/DateContext";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
@@ -27,22 +29,30 @@ function SelectedDay() {
 
   const { isLoading:isUserLoading, data: user } = useQuery("user", getCurrentUser);
 
-  const { isIdle:isOpenTimeblocksIdle , isLoading: isOpenTimeblocksLoading, data: openTimeblocks } = useQuery(
+  const {
+    isIdle: isOpenTimeblocksIdle,
+    isLoading: isOpenTimeblocksLoading,
+    data: openTimeblocks,
+  } = useQuery(
     ["timeblocks", selectedDate, { status: "open" }],
     () => getOpenTimeBlocksByDate(user!.uid, selectedDate),
     {
       enabled: !!user,
     }
   );
-
-  const { isIdle:isClosedTimeblocksIdle, isLoading: isClosedTimeblocksLoading, data: closedTimeblocks } = useQuery(
+  
+  const {
+    isIdle: isClosedTimeblocksIdle,
+    isLoading: isClosedTimeblocksLoading,
+    data: closedTimeblocks,
+  } = useQuery(
     ["timeblocks", selectedDate, { status: "closed" }],
     () => getClosedTimeBlocksByDate(user!.uid,selectedDate),
     {
       enabled: !!user,
     }
   );
-
+  
   const queryClient = useQueryClient();
 
   const statusUpdateMutation = useMutation(updateTimeblockById, {
@@ -70,14 +80,21 @@ function SelectedDay() {
     return date.format("Do MMMM YYYY");
   };
 
-  if (statusUpdateMutation.isLoading || isUserLoading ||  isOpenTimeblocksIdle || isClosedTimeblocksIdle ||   isClosedTimeblocksLoading || isOpenTimeblocksLoading) {
+  if (
+    statusUpdateMutation.isLoading ||
+    isUserLoading ||
+    isOpenTimeblocksIdle ||
+    isClosedTimeblocksIdle ||
+    isClosedTimeblocksLoading ||
+    isOpenTimeblocksLoading
+  ) {
     return (
-      <div className="mt-4 border-black">
+      <div className="mt-4 bg-genesis-gray-200 px-4 py-4 rounded-xl border-black">  
         <h1 className="text-2xl font-semibold">{getHeadingForDate(selectedDate)}</h1>
         <div className="mt-4 ">
           <NavLink to={"/home/timeblocks/create"}>
-            <div className="border border-black flex justify-center px-2 py-2 rounded">
-              <PlusCircleIcon width={20} height={20} />
+          <div className="border-black flex justify-center px-2 py-2 rounded-1.5xl bg-genesis-green-300">
+              <PlusCircleIcon width={20} height={20} className="text-white" />  
             </div>
           </NavLink>
 
@@ -90,48 +107,66 @@ function SelectedDay() {
   }
 
   return (
-    <div className="mt-4 border-black">
-      <h1 className="text-2xl font-semibold cursor-pointer">{getHeadingForDate(selectedDate)}</h1>
-      <div className="mt-4 ">
+    <div className="mt-4  bg-genesis-gray-200 px-4 py-4 rounded-xl border-black">
+    <h1 className="text-2xl font-semibold  ">{getHeadingForDate(selectedDate)}</h1>
+    <div className="mt-4"> 
         <NavLink to={"/home/timeblocks/create"}>
-          <div className="border border-black flex justify-center px-2 py-2 rounded">
-            <PlusCircleIcon width={20} height={20} />
+        <div className=" border-black flex justify-center px-2 py-2 rounded-1.5xl bg-genesis-green-300">
+            <PlusIcon width={20} height={20} className="text-white" /> 
           </div>
         </NavLink>
 
         <div>
-          <h1 className="text-lg mt-4 font-medium">Open</h1>
-          <TimeblockCards statusUpdateMutation={statusUpdateMutation} timeblocks={openTimeblocks} getFormattedTime={getFormattedTime} />
+        <div className="mt-4  px-4 bg-white py-4 rounded-2xl border-black">
+            <h1 className="text-base font-medium">Open Timeblocks</h1>
+            <TimeblockCards
+              statusUpdateMutation={statusUpdateMutation}
+              timeblocks={openTimeblocks}
+              getFormattedTime={getFormattedTime}
+            />
+          </div>  
 
-          <h1 className="text-lg mt-4 font-medium">Closed</h1>
-          <TimeblockCards statusUpdateMutation={statusUpdateMutation} timeblocks={closedTimeblocks} getFormattedTime={getFormattedTime} />
+          <div className="mt-4 px-4 bg-white py-4 rounded-2xl border-black">
+            <h1 className="text-base font-medium">Closed Timeblocks</h1>
+            <TimeblockCards
+              statusUpdateMutation={statusUpdateMutation}
+              timeblocks={closedTimeblocks}
+              getFormattedTime={getFormattedTime}
+            />
+            </div>
+          </div>  
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 function TimeblockCards({
   timeblocks,
   getFormattedTime,
-  statusUpdateMutation
+  statusUpdateMutation,
 }: {
   timeblocks: any;
   getFormattedTime: (dateTime: string) => string;
-  statusUpdateMutation:any
+  statusUpdateMutation: any;
 }) {
-
   if (timeblocks.length === 0) {
     return (
-      <div className="grid grid-cols-1 gap-y-2 mt-2 border-black">
-        <h1>No timeblocks to view</h1>
+      <div className="grid grid-cols-1 place-items-center gap-y-2 mt-2 border-black">
+      <h1 className="text-xs">No timeblocks to view</h1>  
       </div>
     );
   }
   return (
     <div className="grid grid-cols-1 gap-y-2 mt-2 border-black">
       {timeblocks.map((timeblock: any) => {
-        return <TimeBlockCard statusUpdateMutation={statusUpdateMutation}  key={timeblock.id} timeblock={timeblock} getFormattedTime={getFormattedTime} />;
+    return (
+      <TimeBlockCard
+        statusUpdateMutation={statusUpdateMutation}
+        key={timeblock.id}
+        timeblock={timeblock}
+        getFormattedTime={getFormattedTime}
+      />
+    ); 
       })}
     </div>
   );
@@ -140,29 +175,32 @@ function TimeblockCards({
 function TimeBlockCard({
   timeblock,
   getFormattedTime,
-  statusUpdateMutation
+  statusUpdateMutation,
 }: {
   timeblock: any;
   getFormattedTime: (dateTime: string) => string;
-  statusUpdateMutation:any
+  statusUpdateMutation:any;
 }) {
   const navigate = useNavigate();
 
   return (
-    <div key={timeblock.id} className="border  flex justify-between border-black items-center px-4 py-2 rounded">
+    <div
+    key={timeblock.id}
+    className=" px-4 py-4 bg-genesis-gray-700 flex justify-between border-black items-center  rounded-2xl"
+  >
       <div className="flex items-center gap-x-2">
-        <RadioBtn timeblock={timeblock} statusUpdateMutation={statusUpdateMutation}/>
-        <span className={clsx("block text-base", { "line-through": timeblock.status === "closed" })}>
+      <RadioBtn timeblock={timeblock} statusUpdateMutation={statusUpdateMutation} />
+        <span className={clsx("block text-genesis-gray-500 text-sm", { "line-through": timeblock.status === "closed" })}>
           {timeblock.name}
         </span>
       </div>
       <div className="flex items-center">
-        <span className={clsx("text-base", { "line-through": timeblock.status === "closed" })}>{`${getFormattedTime(
+      <span className={clsx("text-xs text-genesis-purple-300", { "line-through": timeblock.status === "closed" })}>{`${getFormattedTime(
           timeblock.s
         )} - ${getFormattedTime(timeblock.e)}`}</span>
         <ChevronRightIcon
           width={20}
-          className="cursor-pointer"
+          className="text-genesis-gray-500 cursor-pointer"
           height={20}
           onClick={() => {
             navigate(`/home/timeblocks/${timeblock.id}`);
@@ -173,9 +211,7 @@ function TimeBlockCard({
   );
 }
 
-function RadioBtn({ timeblock,statusUpdateMutation }: { timeblock: any,statusUpdateMutation:any }) {
-  
-
+function RadioBtn({ timeblock, statusUpdateMutation }: { timeblock: any; statusUpdateMutation: any }) {
   const handleClickWhenOpen = () => {
     statusUpdateMutation.mutate({ ...timeblock, status: "closed" });
   };
@@ -185,11 +221,11 @@ function RadioBtn({ timeblock,statusUpdateMutation }: { timeblock: any,statusUpd
   };
 
   if (timeblock.status === "closed") {
-    return <CheckCircleIcon width={20} height={20} className="cursor-pointer" onClick={handleClickWhenClosed} />;
+    return <CheckCircleIcon width={20} height={20} className="cursor-pointer text-genesis-gray-500" onClick={handleClickWhenClosed} />;
   }
   return (
     <span
-      className="w-4 h-4 rounded-full cursor-pointer block border box-border border-black bg-blackborder-black"
+      className="w-4 h-4 rounded-full  cursor-pointer block border box-border border-genesis-gray-500 bg-blackborder-black"
       onClick={handleClickWhenOpen}
     ></span>
   );

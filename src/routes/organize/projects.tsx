a8e61d445e00ebfcb4b10e88c30e08bd80676b39
Gayali Alahakoon <124614806+GayaliAlahakoon@users.noreply.github.com>
2023-03-31 22:00:41 +0530

    ventures high fid changes

--- src/routes/organize/projects.tsx
+++ src/routes/organize/projects.tsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { PlusCircleIcon, ChevronRightIcon,PlusIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon, ChevronRightIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { useQuery } from "react-query";
 import { getAllProjects, getAllVentures } from "../../api/api";
 import { getCurrentUser } from "../../firebase/auth";
@@ -31,12 +31,11 @@ function Projects() {
       <div className=" bg-genesis-gray-200 px-4 py-4 rounded-xl border-black">
         <h1 className="text-2xl font-semibold">Projects</h1>
         <div className="mt-4">
-          <NavLink to={"/organize/projects/create"}>
-            <div className="border border-black flex justify-center px-2 py-2 rounded">
-              <PlusCircleIcon width={20} height={20} />
-            </div>
-            <div className="grid grid-cols-1 gap-y-2 mt-4 border-black"></div>
-          </NavLink>
+        <NavLink to={"/organize/projects/create"}>
+          <div className="border-black flex justify-center px-2 py-2 rounded-1.5xl bg-genesis-green-300">
+            <PlusIcon width={20} height={20} className="text-white" />
+          </div>
+        </NavLink>
         </div>
         <div className="grid grid-cols-1 place-items-center border-black mt-2">
           <span className="block">Loading</span>
--- src/routes/organize/ventures.tsx
+++ src/routes/organize/ventures.tsx
@@ -1,4 +1,4 @@
-import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { PlusCircleIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { useQuery } from "react-query";
 import { NavLink, useNavigate } from "react-router-dom";
 import { getAllVentures } from "../../api/api";
@@ -8,25 +8,23 @@ import { getCurrentUser } from "../../firebase/auth";
 function Ventures() {
   const navigate = useNavigate();
 
-  const { isLoading:isUserLoading, data: user } = useQuery("user", getCurrentUser);
+  const { isLoading: isUserLoading, data: user } = useQuery("user", getCurrentUser);
 
-
-  const { isLoading, isError, data, error, isSuccess } = useQuery("ventures", ()=> getAllVentures(user!.uid),{
-    enabled:!!user
+  const { isLoading, isError, data, error, isSuccess } = useQuery("ventures", () => getAllVentures(user!.uid), {
+    enabled: !!user,
+  
   });
-
   if (isUserLoading || isLoading) {
     return (
-      <div className=" border-black mt-4">
-        <h1 className="text-2xl font-semibold">Projects</h1>
+      <div className=" mt-4 bg-genesis-gray-200 px-4 py-4 rounded-xl border-black">
+      <h1 className="text-2xl font-semibold">Ventures</h1>
         <div className="mt-4">
           <NavLink to={"/home/timeblocks/create"}>
-            <div className="border border-black flex justify-center px-2 py-2 rounded">
-              <PlusCircleIcon width={20} height={20} />
-            </div>
-            <div className="grid grid-cols-1 gap-y-2 mt-4 border-black"></div>
-          </NavLink>
-        </div>
+          <NavLink to={"/organize/ventures/create"}>
+          <div className="border-black flex justify-center px-2 py-2 rounded-1.5xl bg-genesis-green-300">
+            <PlusIcon width={20} height={20} className="text-white" />
+          </div>
+        </NavLink>  
         <div className="grid grid-cols-1 place-items-center border-black mt-2">
           <span className="block">Loading</span>
         </div>
@@ -35,16 +33,16 @@ function Ventures() {
   }
 
   return (
-    <div className=" border-black mt-4">
+    <div className=" mt-4 bg-genesis-gray-200 px-4 py-4 rounded-xl border-black">
       <h1 className="text-2xl font-semibold">Ventures</h1>
       <div className="mt-4">
         <NavLink to={"/organize/ventures/create"}>
-          <div className="border border-black flex justify-center px-2 py-2 rounded">
-            <PlusCircleIcon width={20} height={20} />
+        <div className="border-black flex justify-center px-2 py-2 rounded-1.5xl bg-genesis-green-300">
+            <PlusIcon width={20} height={20} className="text-white" />
           </div>
         </NavLink>
       </div>
-      <div className="grid grid-cols-1 gap-y-2 mt-4 border-black">
+      <div className="border-black px-4 py-4 bg-white rounded-xl mt-4 grid grid-cols-1 gap-y-2">
         {data.map((venture: any) => {
           return (
             <div
@@ -52,7 +50,7 @@ function Ventures() {
                 navigate(`/organize/ventures/${venture.id}/projects`);
               }}
               key={venture.id}
-              className="border cursor-pointer flex justify-between border-black items-center px-4 py-2 rounded"
+              className="cursor-pointer rounded-2xl px-4 py-4 bg-genesis-gray-700 flex justify-between border-black items-center"
             >
               <span className="block text-base">{venture.name}</span>
               <div className="flex items-center">

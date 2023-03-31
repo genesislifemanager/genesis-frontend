b670aa4132b80a21cea7ea42f8daa93a0cc21b3b
Gayali Alahakoon <124614806+GayaliAlahakoon@users.noreply.github.com>
2023-03-31 23:15:45 +0530

    auth pages high fid changes

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -6,7 +6,6 @@ function SignIn() {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const navigate = useNavigate();
 
-
   const handleFormDataChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -19,34 +18,34 @@ function SignIn() {
 
   return (
     <div className="root-layout border border-black py-16 px-4">
-      <h1 className="text-center font-semibold text-2xl">Sign In Your Account</h1>
+      <h1 className="text-center font-semibold text-2xl">Sign in to your account</h1>
       <div className="mt-4 py-4 grid grid-cols-1 gap-y-4">
-        
         <input
           type="email"
           onChange={handleFormDataChange}
           name="email"
-          className="border invalid:border-red-500 border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Email Address"
         />
         <input
           type="password"
           onChange={handleFormDataChange}
           name="password"
-          className="border border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Password"
         />
-        
-        <button
-          
-          onClick={handleSubmit}
-          className="border border-black w-full px-4 py-2 rounded font-semibold"
-        >
-          Sign In
-        </button>
-      </div>
+
+         <div className="flex justify-center items-center">
+          <button
+            onClick={handleSubmit}
+            className="block rounded-2xl bg-genesis-green-300 text-white border-black w-fit px-16  py-2  font-semibold"
+          >
+            Sign In
+          </button>
+        </div>
+        </div>
       <Link to="/auth/signup" className="block text-center">
-        Dont have an account ? Sign Up
+      Dont have an account ? <span className="underline">Sign Up</span> 
       </Link>
     </div>
   );
--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -30,47 +30,55 @@ function SignUp() {
     <div className="root-layout  border border-black py-16 px-4">
       <h1 className="text-center font-semibold text-2xl">Create an account</h1>
       <div className="mt-4 py-4 grid grid-cols-1 gap-y-4">
+      <h1 className="text-base">Name</h1>
         <input
           type="text"
           onChange={handleFormDataChange}
           name="fname"
-          className="border border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="First Name"
         />
         <input
           type="text"
           onChange={handleFormDataChange}
           name="lname"
-          className="border border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Last Name"
         />
+        <h1 className="text-base">Email</h1>
         <input
           type="email"
           onChange={handleFormDataChange}
           name="email"
-          className="border invalid:border-red-500 border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Email Address"
-        />
+          />
+          <h1 className="text-base">Password</h1>
         <input
           type="password"
           onChange={handleFormDataChange}
           name="password"
-          className="border border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Password"
         />
         <input
           type="password"
           onChange={handleFormDataChange}
           name="confirmPassword"
-          className="border border-black w-full px-4 py-2 rounded"
+          className="border-black w-full px-4 py-2 rounded-lg bg-genesis-gray-200"
           placeholder="Confirm Password"
         />
-        <button onClick={handleSubmit} className="border border-black w-full px-4 py-2 rounded font-semibold">
-          Create Account
-        </button>
+        <div className="flex justify-center items-center">
+          <button
+            onClick={handleSubmit}
+            className="block rounded-2xl bg-genesis-green-300 text-white border-black w-fit px-16  py-2  font-semibold"
+          >
+            Create Account
+          </button>
+        </div>
       </div>
       <Link to="/auth/signin" className="block text-center">
-        Already have an acccount ? Sign In
+      Already have an acccount ? <span className="underline">Sign In</span>
       </Link>
       <p className="text-center text-xs mt-2">
         By creating an account you agree to the <span className="underline">privacy policy</span>

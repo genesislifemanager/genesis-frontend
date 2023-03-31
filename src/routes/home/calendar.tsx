510e5fd656db5b0540d23e06116b271f5ebd8f12
Gayali Alahakoon <124614806+GayaliAlahakoon@users.noreply.github.com>
2023-03-31 18:03:02 +0530

    calendar high fid changes

--- src/routes/home/calendar.tsx
+++ src/routes/home/calendar.tsx
@@ -21,11 +21,12 @@ function Calendar() {
   };
 
   return (
-    <div className="mt-4">
+    <div className="mt-4  border-black px-4 py-4 rounded-xl bg-genesis-gray-200">
       <h1 className="text-2xl font-semibold">Select Date</h1>
       <div className="mt-4 calendar border-black">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <StaticDatePicker
+
             displayStaticWrapperAs="desktop"
             openTo="day"
             value={date}
@@ -41,14 +42,14 @@ function Calendar() {
               navigate(-1);
             }}
             type="button"
-            className="border block border-black rounded px-1 py-1 text-sm font-semibold w-20"
+            className="border-2 text-genesis-gray-800 block border-genesis-gray-800 rounded-lg px-1 py-2 text-sm font-semibold w-20" 
           >
             Cancel
           </button>
           <button
             onClick={handleConfirm}
             type="button"
-            className="border block border-black rounded px-1 py-1 text-sm font-semibold w-20"
+            className=" block border-black bg-genesis-green-300 text-white rounded-lg px-1 py-2 text-sm font-semibold w-20"
           >
             Confirm
           </button>
